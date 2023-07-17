571860c9c794db87a252eab68e86c575e3f91e09
Joseph-Otaget <jotsagepth@gmail.com>
2023-07-17 16:54:57 -0700

    correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 /**
- *main - entry point
- *return: always 0 (success)
- */
+* main - entry point
+*
+* Description:  C program that prints exactly "Programming is like building a multilingual puzzle"
+*
+* return: always 0 (success)
+* /
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
