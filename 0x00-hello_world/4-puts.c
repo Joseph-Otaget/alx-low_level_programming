c6e0afa55a3f0e5859321ff7f1a073a975e7ae44
Joseph-Otaget <jotsagepth@gmail.com>
2023-07-17 17:00:46 -0700

    come and try

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,13 +4,13 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
- * 
+ *
  * Description: C program that prints exactly
  * "Programming is like building a multilingual puzzle"
  */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
