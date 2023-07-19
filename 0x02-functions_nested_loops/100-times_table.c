9c6d0191337701370ef291ff41628e737d1d4968
Joseph-Otaget <jotsagepth@gmail.com>
2023-07-19 14:43:46 -0700

    d

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,35 +6,43 @@
  */
 void print_times_table(int n)
 {
-int row, column, result;
+	int row, column, result;
 
-if (n <= 15 && n >= 0)
-{
-for (row = 0; row <= n; row++)
-{
-for (column = 0; column <= n; column++)
-{
-result = row * column;
+	if (n <= 15 && n >= 0)
+	{
+		for (row = 0; row <= n; row++)
+		{
+			for (column = 0; column <= n; column++)
+			{
+				result = row * column;
 
-if (column != 0)
-{
-_putchar(',');
-_putchar(' ');
+				if (column != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+
+				if (result <= 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(result + '0');
+				}
+				else if (result <= 99)
+				{
+					_putchar(' ');
+					_putchar(result / 10 + '0');
+					_putchar(result % 10 + '0');
+				}
+				else if (result >= 100)
+				{
+					_putchar(result / 100 + '0');
+					_putchar((result / 10) % 10 + '0');
+					_putchar(result % 10 + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
 
-if (result <= 9 && column != 0)
-_putchar(' ');
-
-if (result <= 99 && column != 0)
-_putchar(' ');
-
-if (result >= 100)
-{
-_putchar('0' + (result / 100));
-_putchar('0' + ((result / 10) % 10));
-}
-_putchar('\n');
-}
-}
-}
-}
